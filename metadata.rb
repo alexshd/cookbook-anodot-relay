4428e01fd68777d9123aa6449a2d2c994dadd81f
Alex Shadrin <alex@shdlabs.com>
2014-11-11 00:44:37 +0200

    metadata update

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,8 @@ description      'Installs/Configures anodot-relay'
 long_description 'Installs/Configures anodot-relay'
 version          '0.1.0'
 
+supports 'ubuntu', ">= 14.0"
+
 depends 'apt'
 depends 'nodejs'
 depends 'runit'
