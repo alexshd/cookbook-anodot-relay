d136631d21ab7f900ee9a9e0a913847e7ede997c
shaylang <shay@anodot.com>
2015-01-15 13:07:09 +0200

    add support for filter and rewrite

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,9 @@
 default['anodot']['token'] = "<YOUR ANODOT TOKEN>"
 default['anodot']['loglevel'] = "error"
 default['anodot']['showStats'] = false
+default['anodot']['sendStats'] = true
 default['anodot']['plainPort'] = 2003
 default['anodot']['picklePort'] = 2004
 default['anodot']['flushInterval'] = 5000
+default['anodot']['metricFilter'] = ""
+default['anodot']['metricRewrite'] = [] 
