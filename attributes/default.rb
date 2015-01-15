f58c7dabd8725d9845346ac392680052a1b72cca
Shalom Yerushalmy <shalom@everything.me>
2015-01-15 12:13:50 +0200

    Added sendStats option

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,3 +4,4 @@ default['anodot']['showStats'] = false
 default['anodot']['plainPort'] = 2003
 default['anodot']['picklePort'] = 2004
 default['anodot']['flushInterval'] = 5000
+default['anodot']['sendStats'] = true
